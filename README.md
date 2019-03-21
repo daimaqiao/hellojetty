a50a62c67b2ff8a33eed0e1b5e167d7a27b2e78c
daimaqiao <daimaqiao@126.com>
2019-03-21 11:37:17 +0800

    更新到Jetty 9.4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hellojetty
 基于Jetty的可执行jar包
+
+更新到Jetty 9.4
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.io.File;
@@ -14,7 +15,7 @@ public class Main {
         // 指定war包之后，xxx.jar的作用就是一个start.jar的作用，即：启动jetty，并加载xxx.war，开始服务。
         final String war= System.getProperty("war", "hellojetty.war");
 
-        // 获取代码运行位置，在调试阶段可能是一个classes目录，打成jar包之后，谅是是那个jar包的位置
+        // 获取代码运行位置，在调试阶段可能是一个classes目录，打成jar包之后，是那个jar包的位置
         URL location= Main.class.getProtectionDomain().getCodeSource().getLocation();
         File home= new File(location.getFile());
         System.out.println("home: "+ home);
@@ -31,7 +32,7 @@ public class Main {
         context.setWar(warfile.getAbsolutePath());
 
         /*
-        // Jetty 9.3.x 和以后的版本需要额外设置container，instance manager等属性
+        // 适用于Jetty 9.3.x：为了支持jsp，需要额外设置container，instance manager等属性
         // 具体可以参考下面文章或者官方示例代码
         // https://segmentfault.com/q/1010000006126591
         // https://github.com/jetty-project/embedded-jetty-jsp
@@ -45,6 +46,18 @@ public class Main {
         */
 
         Server server= new Server(port);
+
+        // 适用于Jetty 9.4.x：参考以下官方示例，主要还是为了支持jsp
+        // https://github.com/puppetlabs/trapperkeeper-webserver-jetty9/issues/140
+        // https://www.eclipse.org/jetty/documentation/current/embedded-examples.html#embedded-webapp-jsp
+        Configuration.ClassList classList= Configuration.ClassList
+                .setServerDefault(server);
+        classList.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+                "org.eclipse.jetty.annotations.AnnotationConfiguration");
+        context.setAttribute(
+                "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+                ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/[^/]*taglibs.*\\.jar$");
+
         server.setHandler(context);
         server.start();
         server.join();
